3cf25323843fd24529c05dee185b0521b344f325
Ishimwe7 <ishimweinstein@gmail.com>
2025-05-24 15:18:19 +0200

    edited testimonials

--- src/utils/TestimonialData.tsx
+++ src/utils/TestimonialData.tsx
@@ -14,19 +14,19 @@ export const TestimonialData = [
     name: 'Aline Umutoni',
     initials: 'AU',
     rating: 5,
-    text: `Natangiye kwiga ibyerekeye provisoire kuri iyi website mu gihe cy’ukwezi. 
-             Nyuma y'uko niga neza amasomo n’inyigisho zihari, nabashije kumenya 
-             ibibazo bisanzwe bibazwa mu kizamini. Ibi byatumye natsinda kizamini cya 
-             provisoire nta nkomyi. Ndashimira iyi website ku bufasha bwayo bw’indashyikirwa!`,
+    text: `Nari mfite impungenge ku buryo niteguraga ikizamini cya provisoire.
+     Twaje kubona iyi website, dutangira gukoresha amasomo yayo buri munsi.
+      Kubera uburyo amasomo ateguye neza kandi asobanutse, nize afite icyizere.
+       natsinze ku nshuro ya mbere! Nubwo ari ikizamini gikomeye, iyi platform yaramfashije cyane!`,
   },
   {
     id: 3,
     name: 'John Ngabo',
     initials: 'JN',
     rating: 5,
-    text: `Natangiye kwiga ibyerekeye provisoire kuri iyi website mu gihe cy’ukwezi. 
-             Nyuma y'uko niga neza amasomo n’inyigisho zihari, nabashije kumenya 
-             ibibazo bisanzwe bibazwa mu kizamini. Ibi byatumye natsinda kizamini cya 
-             provisoire nta nkomyi. Ndashimira iyi website ku bufasha bwayo bw’indashyikirwa!`,
+    text: `Kubera akazi kenshi mfite, sinabashaga kubona umwanya uhagije wo kujya mu ishuri.
+     Iyi website yampaye amahirwe yo kwiga igihe cyose mfite akanya, haba nijoro cyangwa mu biruhuko.
+      Nize amasomo yose atandukanye, nkorera n’ibizamini bya test.
+     Byaranyoroheye gutahura ibibazo bikunda kubazwa. Ubu mfite uruhushya rwa provisoire!`,
   },
 ];
