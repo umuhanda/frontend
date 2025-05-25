5c1f01de88dee55de8f2da109cb710b17929d064
Ishimwe7 <ishimweinstein@gmail.com>
2025-05-26 01:18:22 +0200

    changed pricing plans

--- src/utils/PricingPlans.ts
+++ src/utils/PricingPlans.ts
@@ -2,9 +2,12 @@ const BASIC_KINYARWANDA = import.meta.env.VITE_KINYARWANDA_BASIC as string;
 const GENERAL_KINYARWANDA = import.meta.env.VITE_KINYARWANDA_GENERAL as string;
 const PREMIUM_KINYARWANDA = import.meta.env.VITE_KINYARWANDA_PREMIUM as string;
 const PRO_KINYARWANDA = import.meta.env.VITE_KINYARWANDA_PRO as string;
-const JUNIOR_EN_FR = import.meta.env.VITE_JUNIOR_EN_FR as string;
-const SENIOR_EN_FR = import.meta.env.VITE_SENIOR_EN_FR as string;
-const PREMIUM_EN_FR = import.meta.env.VITE_PREMIUM_EN_FR as string;
+const JUNIOR_EN = import.meta.env.VITE_JUNIOR_EN as string;
+const SENIOR_EN = import.meta.env.VITE_SENIOR_EN as string;
+const PREMIUM_EN = import.meta.env.VITE_PREMIUM_EN as string;
+const JUNIOR_FR = import.meta.env.VITE_JUNIOR_FR as string;
+const SENIOR_FR = import.meta.env.VITE_SENIOR_FR as string;
+const PREMIUM_FR = import.meta.env.VITE_PREMIUM_FR as string;
 
 export const PricingPlans = [
   {
@@ -37,42 +40,42 @@ export const PricingPlans = [
   },
   {
     id: 5,
-    subId: JUNIOR_EN_FR,
+    subId: JUNIOR_EN,
     name: 'junior',
     price: 1000,
     language: 'English',
   },
   {
     id: 6,
-    subId: SENIOR_EN_FR,
+    subId: SENIOR_EN,
     name: 'senior',
     price: 10000,
     language: 'English',
   },
   {
     id: 7,
-    subId: PREMIUM_EN_FR,
+    subId: PREMIUM_EN,
     name: 'principal',
     price: 15000,
     language: 'English',
   },
   {
     id: 8,
-    subId: JUNIOR_EN_FR,
+    subId: JUNIOR_FR,
     name: 'beginner',
     price: 1000,
     language: 'French',
   },
   {
     id: 9,
-    subId: SENIOR_EN_FR,
+    subId: SENIOR_FR,
     name: 'experienced',
     price: 10000,
     language: 'French',
   },
   {
     id: 10,
-    subId: PREMIUM_EN_FR,
+    subId: PREMIUM_FR,
     name: 'master',
     price: 15000,
     language: 'French',
